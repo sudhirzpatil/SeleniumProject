0a544bb4ec1005380d94e10e36f5ca45452cc49c
sudhir_patil <sudhir_patil@PTD23414.persistent.co.in>
2018-11-27 11:34:38 +0530

    Test by sudhir on 11.34

--- WebdriverTest/src/main/java/com/trinity/pages/CertificationPage.java
+++ WebdriverTest/src/main/java/com/trinity/pages/CertificationPage.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
-//import org.openqa.selenium.StaleElementReferenceException;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
