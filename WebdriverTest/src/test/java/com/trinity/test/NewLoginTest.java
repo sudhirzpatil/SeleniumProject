ef444ffdcf4911e3d168b1f9e27294ab9793dbaf
sudhir_patil <sudhir_patil@PTD23414.persistent.co.in>
2018-12-13 15:33:03 +0530

    New test file added

---
+++ WebdriverTest/src/test/java/com/trinity/test/NewLoginTest.java
@@ -0,0 +1,31 @@
+package com.trinity.test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.trinity.pages.NewLoginPage;
+import com.util.impl.BaseScript;
+
+public class NewLoginTest extends BaseScript {
+	
+	NewLoginPage objLogin;
+	
+	@BeforeTest
+	public void setup(){
+		
+		killAllWindows();
+		setupConfig();
+		setupDriver();
+	}
+	
+	@Test(priority=0)
+	public void test_Login_To_Trinity(){
+		
+		objLogin = new NewLoginPage(driver);
+		objLogin.loginToGuru99();
+	}
+	
+	
+	
+
+	
+}
